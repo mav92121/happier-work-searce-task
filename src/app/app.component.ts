af5873a6d0a955510889bb24602676a9f8217135
Mohsinali Vijapura <mohsinali.vijapura@shipmnts.com>
2024-08-24 10:06:28 +0530

    login page completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzModalModule } from 'ng-zorro-antd/modal';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NzButtonModule, NzModalModule,FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'happier_work';
+  // showModal = false;
+  // constructor() {}
+  // handleCancel() {
+  //   this.showModal = false;
+  // }
+  // handleOk() {
+  //   this.showModal = false;
+  // }
 }
