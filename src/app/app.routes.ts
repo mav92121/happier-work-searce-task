42159f92df24537c08b71617625c5e99f7355145
Mohsinali Vijapura <mohsinali530121@gmail.com>
2024-08-24 17:16:19 +0530

    assets and styles updted

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './components/login/login.component';
+import { HeaderLayoutComponent } from './components/header-layout/header-layout.component';
+import { SidebarLayoutComponent } from './components/sidebar-layout/sidebar-layout.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 export const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
   },
 ];
